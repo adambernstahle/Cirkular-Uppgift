4d026d3807994cdf8cc1169591e450f534068c65
Tomas Landberg <landbergaren@gmail.com>
2017-10-18 00:47:26 +0200

    Jag fixade så uppgiften blev mer OOP plus att klassen blev som en hjälpklass för att sätta in värde och hämta. Allt vi gör med värdena gör vi i program-classen. Plus att jag skapade en fil för Queue classen. Plus att nu funkar uppgiften som den ska i stort sett :D

--- Cirkulär Buffer Uppgift Adam Bernståhle/Program.cs
+++ Cirkulär Buffer Uppgift Adam Bernståhle/Program.cs
@@ -6,104 +6,36 @@ namespace Cirkulär_Buffer_Uppgift
     {
         static void Main(string[] args)
         {
-            Queue q = new Queue(5);
+            Queue q = new Queue(15);
             while (true)
             {
                 char userInput;
 
-                Console.WriteLine("Vill du skriva (i)n eller (h)ämta?");
+                Console.WriteLine("\nVill du skriva (i)n eller (h)ämta?");
                 userInput = Console.ReadKey().KeyChar;
+
                 if (userInput == 'i')
                 {
+                    int inputValue;
+                    Console.WriteLine("\nWrite a number");
+                    while (!(int.TryParse(Console.ReadLine(), out inputValue)))
+                        Console.WriteLine("\nUse correct digits");
 
-                    q.Enqueue();
-                    Console.WriteLine();
+                    q.Enqueue(inputValue);
+
                 }
                 else if (userInput == 'h')
                 {
-                    q.Dequeue();
+                    int tailValue = 0;
+                    Console.WriteLine();
+                    if (q.Dequeue(ref tailValue))
+                        Console.WriteLine(tailValue);
                 }
                 else
-                    Console.WriteLine("Välj i eller h");
+                    Console.WriteLine("\nVälj i eller h");
 
 
             }
-        }
-    }
-    class Queue
-    {
-        public int[] buf;
-        private int head;
-        private int tail;
-
-        public Queue(int n)
-        {
-            head = 0;
-            tail = 0;
-            buf = new int[n];
-        }
-
-        public void Enqueue()
-        {
-            head++;
-            if (head >= buf.Length)
-                head = 0;
-            if (Full())
-            {
-                Console.WriteLine("Buffer är full");
-                head--;
-            }
-            else
-            {
-                bool trueFalse = false;
-
-                while (!trueFalse)
-                {
-                    Console.Write("\nVad vill du skriva in? ");
-                    trueFalse = int.TryParse(Console.ReadLine(), out int value);
-                    buf[head] = value;
-                }
-
-            }
-        }
-
-        public void Dequeue()
-        {
-
-            tail++;
-            if (tail >= buf.Length)
-                tail = 0;
-
-            if (Empty())
-            {
-                Console.WriteLine("\nBuffer är tom!");
-                tail--;
-            }
-
-            else
-            {
-                Console.WriteLine();
-                Console.WriteLine(buf[tail]);
-                buf[tail] = 0; // TODO Reset value, look up NULL
-
-            }
-        }
-
-
-        private bool Full()
-        {
-            if (tail == (head % buf.Length))
-                return true;
-            else
-                return false;
-        }
-        private bool Empty()
-        {
-            if (head == (tail % buf.Length))
-                return true;
-            else
-                return false;
         }
     }
 }
-
---
+++ Cirkulär Buffer Uppgift Adam Bernståhle/Queue.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cirkulär_Buffer_Uppgift
+{
+    class Queue
+    {
+        public int[] buf;
+        private int head;
+        private int tail;
+        int take;
+
+        public Queue(int n)
+        {
+            head = 0;
+            tail = 0;
+            buf = new int[n];
+        }
+
+        public bool Enqueue(int put)
+        {
+            if (Full())
+            {
+                Console.WriteLine("\nBuffert is full");
+                return false;
+            }
+            else
+            {
+                buf[head] = put;
+                
+                head = (head + 1) % buf.Length;
+                
+            }
+            return true;
+        }
+
+        public bool Dequeue(ref int take)
+        {
+            if (Empty())
+            {
+                Console.WriteLine("\nBuffert is empty");
+                return false;
+            }
+            else
+            {
+                take = buf[tail];
+
+                tail = (tail + 1) % buf.Length;
+                return true;
+            }
+        }
+        
+        private bool Full()
+        {
+            if (tail == ((head + 1) % buf.Length))
+                return true;
+            else
+                return false;
+        }
+        private bool Empty()
+        {
+            if (head == tail)
+                return true;
+            else
+                return false;
+        }
+    }
+}
+
