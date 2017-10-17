635f3f1609ab8012462aa8f038cdc859bbe0cca3
adambernstahle <adam.bernstahle@gmail.com>
2017-10-17 14:01:02 +0200

    1

--- Cirkulär Buffer Uppgift Adam Bernståhle/Program.cs
+++ Cirkulär Buffer Uppgift Adam Bernståhle/Program.cs
@@ -1,12 +1,109 @@
 ﻿using System;
 
-namespace Cirkulär_Buffer_Uppgift_Adam_Bernståhle
+namespace Cirkulär_Buffer_Uppgift
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue q = new Queue(5);
+            while (true)
+            {
+                char userInput;
+
+                Console.WriteLine("Vill du skriva (i)n eller (h)ämta?");
+                userInput = Console.ReadKey().KeyChar;
+                if (userInput == 'i')
+                {
+
+                    q.Enqueue();
+                    Console.WriteLine();
+                }
+                else if (userInput == 'h')
+                {
+                    q.Dequeue();
+                }
+                else
+                    Console.WriteLine("Välj i eller h");
+
+
+            }
+        }
+    }
+    class Queue
+    {
+        public int[] buf;
+        private int head;
+        private int tail;
+
+        public Queue(int n)
+        {
+            head = 0;
+            tail = 0;
+            buf = new int[n];
+        }
+
+        public void Enqueue()
+        {
+            head++;
+            if (head >= buf.Length)
+                head = 0;
+            if (Full())
+            {
+                Console.WriteLine("Buffer är full");
+                head--;
+            }
+            else
+            {
+                bool trueFalse = false;
+
+                while (!trueFalse)
+                {
+                    Console.Write("\nVad vill du skriva in? ");
+                    trueFalse = int.TryParse(Console.ReadLine(), out int value);
+                    buf[head] = value;
+                }
+
+            }
+        }
+
+        public void Dequeue()
+        {
+
+            tail++;
+            if (tail >= buf.Length)
+                tail = 0;
+
+            if (Empty())
+            {
+                Console.WriteLine("\nBuffer är tom!");
+                tail--;
+            }
+
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine(buf[tail]);
+                buf[tail] = 0; // TODO Reset value, look up NULL
+
+            }
+        }
+
+
+        private bool Full()
+        {
+            if (tail == (head % buf.Length))
+                return true;
+            else
+                return false;
+        }
+        private bool Empty()
+        {
+            if (head == (tail % buf.Length))
+                return true;
+            else
+                return false;
         }
     }
 }
+
